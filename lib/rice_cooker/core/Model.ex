f9001a1aa76d255b3080ed9726b39d84555a8c59
Yume <hei.hajatiana@gmail.com>
2023-12-17 21:53:30 +0300

    fixup: rice_cooker model

--- lib/rice_cooker/core/Model.ex
+++ lib/rice_cooker/core/Model.ex
@@ -1,5 +1,10 @@
 defmodule RiceCooker.Core.Model.RiceCooker do
   @default_capacity 20
 
-  defstruct capacity: @default_capacity
+  defstruct capacity: @default_capacity,
+            is_lid_open: false,
+            is_plugged: false,
+            is_ready: false,
+            water_cup: 0,
+            rice_cup: 0
 end
